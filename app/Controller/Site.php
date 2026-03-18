f34aa003a1158693f64815ff156a4117267808b4
pashechkar-max <polinausakova921@gmail.com>
2026-03-18 22:20:17 +0700

    auth

--- app/Controller/Site.php
+++ app/Controller/Site.php
@@ -6,6 +6,7 @@ use Model\Post;
 use Src\View;
 use Src\Request;
 use Model\User;
+use Src\Auth\Auth;
 
 class Site
 {
@@ -20,13 +21,42 @@ class Site
         return new View('site.hello', ['message' => 'hello working']);
     }
 
+//    public function signup(Request $request): string
+//    {
+//        if ($request->method==='POST' && User::create($request->all())){
+//            return new View('site.signup', ['message'=>'Вы успешно зарегистрированы']);
+//        }
+//        return new View('site.signup');
+//    }
+
     public function signup(Request $request): string
     {
-        if ($request->method==='POST' && User::create($request->all())){
-            return new View('site.signup', ['message'=>'Вы успешно зарегистрированы']);
+        if ($request->method === 'POST' && User::create($request->all())) {
+            app()->route->redirect('/go');
         }
         return new View('site.signup');
     }
+
+    public function login(Request $request): string
+    {
+        //Если просто обращение к странице, то отобразить форму
+        if ($request->method === 'GET') {
+            return new View('site.login');
+        }
+        //Если удалось аутентифицировать пользователя, то редирект
+        if (Auth::attempt($request->all())) {
+            app()->route->redirect('/hello');
+        }
+        //Если аутентификация не удалась, то сообщение об ошибке
+        return new View('site.login', ['message' => 'Неправильные логин или пароль']);
+    }
+
+    public function logout(): void
+    {
+        Auth::logout();
+        app()->route->redirect('/hello');
+    }
+
 
 }
 
--- core/bootstrap.php
+++ core/bootstrap.php
@@ -5,6 +5,7 @@ const DIR_CONFIG = '/../config';
 //Подключение автозагрузчика composer
 require_once __DIR__ . '/../vendor/autoload.php';
 
+
 //Функция, возвращающая массив всех настроек приложения
 function getConfigs(string $path = DIR_CONFIG): array
 {
@@ -18,6 +19,12 @@ function getConfigs(string $path = DIR_CONFIG): array
     return $settings;
 }
 
-require_once __DIR__ . '/../routes/web.php';
 
-return new Src\Application(new Src\Settings(getConfigs()));
+require_once __DIR__ . '/../routes/web.php';
+$app = new Src\Application(new Src\Settings(getConfigs()));
+
+function app() {
+    global $app;
+    return $app;
+}
+return $app;
